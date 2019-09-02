33dd4721819eae77348e67fed3405370b7194b1e
ak-nkmr <centervil.en@gmail.com>
2019-09-03 01:50:03 +0900

    ViewModelを追加して、TextにDataBinding

---
+++ MVVMDemo/MVVMDemo.WpfApp/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMDemo.WpfApp
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string nowTimeText = "initial";
+
+        public MainWindowViewModel()
+        {
+        }
+
+        public string NowTimeText
+        {
+            get
+            {
+                return nowTimeText;
+            }
+            set
+            {
+                nowTimeText = value;
+                OnPropertyChanged();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
